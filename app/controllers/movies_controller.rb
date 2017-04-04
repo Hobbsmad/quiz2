aa22943ebfc10f54d7fceac0c28e361e12a9ca82
Madelyn Hobbs <madelynhobbs@Madelyns-MacBook-Pro.local>
2017-04-03 19:48:33 -0500

    added some details

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,22 @@ class MoviesController < ApplicationController
   def index
     @movie = Movie.order("RANDOM()").first
   end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.create(movie_params)
+  if @movie.invalid?
+    flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
+  end
+    redirect_to root_path
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :director)
+  end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,4 @@
 class Movie < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 140, minimum: 3 }
+  validates :director, presence: true, length: { maximum: 50, minimum: 3 }
 end
